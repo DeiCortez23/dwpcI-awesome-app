a7eab033f6c6edc17e42e4942bc2c60f7f9e426e
DeiCortez23 <132030147+DeiCortez23@users.noreply.github.com>
2024-05-30 19:21:25 +0000

    🏗️ avance template engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Importando Express
 import express from 'express';
 import httpStatus from 'http-status';
 
@@ -7,7 +8,14 @@ import { engine } from 'express-handlebars';
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
-@@ -16,6 +19,23 @@ import path from 'path';
+
+// Importando el directorio raiz
+import { ROOT_DIR } from './helpers/paths.js'
+
+// Se importa path
+import path from 'path';
+
+// Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
 
@@ -30,3 +38,28 @@ app.set('views', path.resolve('views'));
 
 // Se registra el middleware del body-parser
 app.use(express.urlencoded({ extended: true }));
+
+// Se registra el middleware para el servidor
+// de archivos estaticos
+app.use(express.static(path.join(ROOT_DIR, 'public')));
+
+// Se agrega ruta de administrador
+app.use('/admin', adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
+
+// Registrando el middleware para el error
+// 404
+app.use((req, res, next) => {
+  res.status(httpStatus.NOT_FOUND)
+  .render('404')
+});
+
+// Definiendo puertos
+const port = 3000;
+const ip = "0.0.0.0"
+
+// Arrancando el servidor
+app.listen(port, ip, () => {
+  console.log(`🤖 Sirviendo en http://localhost:${port}`);
+});
